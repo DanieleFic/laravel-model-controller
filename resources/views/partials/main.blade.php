7eaac8cbbaea075fb839988ba732a25d3909595a
DanieleFic <reb_ficus22@hotmail.it>
2022-03-04 16:22:24 +0100

    fatto bonus

--- resources/views/partials/main.blade.php
+++ resources/views/partials/main.blade.php
@@ -1,5 +1,21 @@
 
 <div class="ms_container">
     <h2>LISTA FILM</h2>
-    @dd($movie_dati)
+    {{-- @dd($movie_dati); --}}
+    <div class="ms_containerfilm">
+        @foreach ($movie_dati as $key => $element)
+            {{--  @foreach($element as $film) --}}
+                <div class="ms_boxfilm">
+                    {{-- <img src="{{$element["thumb"]}}" alt=""> --}}
+                    <h3>{{$element->title}}</h3>
+                    <p>{{$element->original_title}}</p>
+                    <p>{{$element->nationality}}</p>
+                    <p>{{$element->date}}</p>
+                    <p>{{$element->vote}}</p>
+                </div>
+            @endforeach
+    </div>
+    
+            
 </div>
+
